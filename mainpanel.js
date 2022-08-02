e56d9e927972e246cf1f0d1e5c88b612e44cbcef
LegendAmber <98574142+LegendAmber@users.noreply.github.com>
2022-08-02 08:27:17 -0700

    Update mainpanel.js

--- mainpanel.js
+++ mainpanel.js
@@ -9,7 +9,7 @@ c.fillStyle = 'black';
 c.fillRect(0, 0, innerWidth, innerHeight);
 c.fill();
 
-//Enters new text
+//Enters text
 c.beginPath();
 c.fillStyle = 'white';
 c.font = 'bold 25px firasans';
@@ -19,3 +19,11 @@ c.fill();
 c.beginPath();
 c.fillText("Game", 110, 32.5);
 c.fill();
+
+//Enters spacing
+c.beginPath();
+c.strokeStyle = 'white';
+c.lineWidth = 5;
+c.moveTo(100, 28);
+c.lineTo(100, 37);
+c.stroke();
