7ccff278770a928cd324728a12b616a0927b012e
LegendAmber <98574142+LegendAmber@users.noreply.github.com>
2022-08-02 07:06:16 -0700

    Update mainpanel.js

--- mainpanel.js
+++ mainpanel.js
@@ -1,1 +1,10 @@
+var canvas = document.querySelector('canvas');
+canvas.height = window.innerHeight / 3;
+canvas.width = window.innerWidth;
+var c = canvas.getContext("2d");
 
+//Path begins
+c.beginPath();
+c.fillStyle = 'black';
+c.fillRect(0, 0, innerWidth, innerHeight);
+c.fill();
