cd5a9d2c46eca96ef773977a7b852cbb31c030b2
LegendAmber <98574142+LegendAmber@users.noreply.github.com>
2022-10-06 13:28:31 -0700

    user

--- userPref.js
+++ userPref.js
@@ -1,6 +1,6 @@
 var image = document.getElementById("afcabe54-5230-4d08-afac-20bbe924209d");
 var i = 1;
-image.addEventListener("click", e => {
+image.addEventListener('click', e => {
   if (i == 1){
   image.src = "sun.png";
   i = 0;
