30a40c31834561d0bfd7a132017b3fec5f30f86f
LegendAmber <98574142+LegendAmber@users.noreply.github.com>
2022-10-06 13:26:41 -0700

    user

--- userPref.js
+++ userPref.js
@@ -1,5 +1,12 @@
 var image = document.getElementById("afcabe54-5230-4d08-afac-20bbe924209d");
-
+var i = 1;
 image.addEventListener("click", e => {
-  console.log("clicked");
+  if (i == 1){
+  image.src = "sun.png";
+  i = 0;
+  }
+  else{
+  image.src = "moon.png";
+  i = 1;
+  }
 });
