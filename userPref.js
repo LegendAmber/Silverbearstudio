42b8f4f2a44ccb1e2d0d8202b5c44dce84b04db1
LegendAmber <98574142+LegendAmber@users.noreply.github.com>
2022-10-06 12:53:35 -0700

    Update userPref.js

--- userPref.js
+++ userPref.js
@@ -1,5 +1,6 @@
 var image = document.getElementById("afcabe54-5230-4d08-afac-20bbe924209d");
 //Code that changes from light/dark mode.
 lightdark() {
-image.src = "sun.png";
+  console.log("Click");
+//image.src = "sun.png";
 }
