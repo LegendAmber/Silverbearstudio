47e4e6772e22a84ef26484f57feb2a014a2c6d60
LegendAmber <98574142+LegendAmber@users.noreply.github.com>
2022-10-06 13:33:38 -0700

    user

--- userPref.js
+++ userPref.js
@@ -1,12 +1,24 @@
 var image = document.getElementById("afcabe54-5230-4d08-afac-20bbe924209d");
+var body = document.querySelector('body');
 var i = 1;
 image.addEventListener("click", e => {
   if (i == 1){
   image.src = "sun.png";
   i = 0;
+  light();
   }
   else{
   image.src = "moon.png";
   i = 1;
+  dark();
   }
 });
+
+function light() {
+body.style.backgroundColor = "white";
+body.style.color = "black";
+}
+function dark() {
+body.style.backgroundColor = "black";
+body.style.color = "white";
+}
