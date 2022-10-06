5441ee556bc6dd8628214947808e08fb6ad71f3c
LegendAmber <98574142+LegendAmber@users.noreply.github.com>
2022-10-06 14:42:55 -0700

    imager

--- imgInfo.js
+++ imgInfo.js
@@ -1,5 +1,10 @@
 var image = document.getElementById("login");
 
 image.addEventListener('mouseover', e => {
+    if (e === true){
 image.src = "Login-0.png";
+    }
+    else {
+        image.src = "Login-0.jpg";
+    }
 });
